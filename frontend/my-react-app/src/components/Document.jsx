23d0c80fbf6b1c0142bbdba8205b4992b682ce6d
spiritoftime <chooliangyi6@gmail.com>
2023-05-04 11:12:21 +0800

    add delete document functionality

--- frontend/my-react-app/src/components/Document.jsx
+++ frontend/my-react-app/src/components/Document.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import Box from "@mui/material/Box";
-
+import { useMutation } from "@tanstack/react-query";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import ArticleIcon from "@mui/icons-material/Article";
 import Typography from "@mui/material/Typography";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { deleteDocument } from "../services/document";
+import { useAppContext } from "../context/appContext";
+import IconButton from "@mui/material/IconButton";
+import { persistLogin } from "../services/auth";
+import { api } from "../services/makeRequest";
 const Document = ({ node }) => {
+  const { setAuthDetails, setIsLoadingAuth } = useAppContext();
+  const { mutate: persistLoginMutation } = useMutation({
+    mutationFn: (accessToken) => {
+      return persistLogin(accessToken);
+    },
+    onSuccess: (res) => {
+      setAuthDetails({ ...res.data.userWithDocuments });
+      const accessToken = res.headers.authorization.split(" ")[1];
+      localStorage.setItem("accessToken", accessToken);
+      api.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+      setIsLoadingAuth(false); // Set loading state to false after checking
+    },
+  });
+  const { mutate: deleteDocumentMutation } = useMutation({
+    mutationFn: (documentId) => {
+      return deleteDocument(documentId);
+    },
+    onSuccess: (res) => {
+      persistLoginMutation(localStorage.getItem("accessToken"));
+    },
+  });
   return (
     <>
       <Box display="flex" alignItems="center">
@@ -20,7 +46,12 @@ const Document = ({ node }) => {
           <ArticleIcon color="primary" />
           {node.title}
         </Typography>
-        <DeleteIcon color="error" />
+        <IconButton
+          onClick={() => deleteDocumentMutation(node.id)}
+          color="error"
+        >
+          <DeleteIcon />
+        </IconButton>
       </Box>
     </>
   );
